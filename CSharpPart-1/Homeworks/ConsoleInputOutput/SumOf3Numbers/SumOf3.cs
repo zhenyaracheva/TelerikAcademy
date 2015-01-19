8c464ae9b16225d53a6fdb412dae272c0f7c63e8
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-19 21:56:09 +0200

    Added problem 1

---
+++ CSharpPart-1/Homeworks/ConsoleInputOutput/SumOf3Numbers/SumOf3.cs
@@ -0,0 +1,30 @@
+﻿//Problem 1. Sum of 3 Numbers
+
+//Write a program that reads 3 real numbers from the console and prints their sum.
+//Examples:
+
+// a	b	    c	    sum
+// 3 	4	    11	    18
+// -2	0	    3	    1
+// 5.5	4.5 	20.1	30.1
+
+using System;
+
+class SumOf3
+{
+    static void Main()
+    {
+        double result = 0;
+
+        Console.WriteLine("Enter 3 numbers: ");
+
+        for (int i = 0; i < 3; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            result += number;
+        }
+
+        Console.WriteLine(result);
+    }
+}
+
