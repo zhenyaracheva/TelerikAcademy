50b4fd6bf03aa0160f68c7610696bcab888910b3
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-21 15:27:14 +0200

    Added Problem 10. Fibonacci Numbers

---
+++ CSharpPart-1/Homeworks/ConsoleInputOutput/FibonacciNumbers/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿//Problem 10. Fibonacci Numbers
+
+//Write a program that reads a number n and prints on the console the first n members of the Fibonacci sequence 
+//(at a single line, separated by comma and space - ,) : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ….
+
+using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        Console.Write("Enter positive number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+       
+        while (n != 0)
+        {
+            Console.Write("{0} ", firstNumber);
+            secondNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber - firstNumber;
+            n--;
+        }
+
+        Console.WriteLine();
+       
+    }
+}
+
