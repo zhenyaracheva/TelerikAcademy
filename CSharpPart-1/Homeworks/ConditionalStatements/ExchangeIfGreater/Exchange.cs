c3f005075fc3b833d0d845e8496c9d2eb955987a
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-21 21:47:30 +0200

    Added Problem 1. Exchange If Greater

---
+++ CSharpPart-1/Homeworks/ConditionalStatements/ExchangeIfGreater/Exchange.cs
@@ -0,0 +1,34 @@
+﻿//Problem 1. Exchange If Greater
+
+//Write an if-statement that takes two double variables a and b and exchanges their values if the first one is greater than the second one.
+//As a result print the values a and b, separated by a space.
+//Examples:
+
+//a	    b	    result
+//5	    2	    2 5
+//3	    4	    3 4
+//5.5	4.5	    4.5 5.5
+
+using System;
+
+class Exchange
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double first = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double second = double.Parse(Console.ReadLine());
+
+        if (second < first)
+        {
+            Console.WriteLine("{0} {1}", second, first);
+        }
+        else
+        {
+            Console.WriteLine("{0} {1}", first, second);
+        }
+    }
+}
+
