10249f245f0efcb9be01e24e8628c341182ca6e0
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-21 13:11:57 +0200

    Added Problem 7. Sum of 5 Numbers

---
+++ CSharpPart-1/Homeworks/ConsoleInputOutput/QuadraticEquation/Quadratic.cs
@@ -0,0 +1,49 @@
+﻿//Problem 6. Quadratic Equation
+
+//Write a program that reads the coefficients a, b and c of a quadratic equation ax2 + bx + c = 0 and solves it (prints its real roots).
+//Examples:
+
+//a	    b	 c	    roots
+//2	    5	 -3	    x1=-3; x2=0.5
+//-1	3	 0	    x1=3; x2=0
+//-0.5	4	 -8	    x1=x2=4
+//5	    2	 8	    no real roots
+
+using System;
+
+class Quadratic
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter coefficients a, b and c of a quadratic equation ax2 + bx + c = 0:");
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("c = ");
+        double c = double.Parse(Console.ReadLine());
+
+        double D = (b * b) - (4 * a * c);
+
+        double x1;
+
+        if (D > 0)
+        {
+            x1 = ((-1) * b - Math.Sqrt(D)) / (2 * a);
+            double x2 = ((-1) * b + Math.Sqrt(D)) / (2 * a); ;
+            Console.WriteLine("x1 = {0}; x2 = {1}", x1, x2);
+        }
+        else if (D == 0)
+        {
+            x1 = -b / (2 * a);
+            Console.WriteLine("x1 = x2 = {0}", x1);
+        }
+        else
+        {
+            Console.WriteLine("no real roots");
+        }
+    }
+}
+
---
+++ CSharpPart-1/Homeworks/ConsoleInputOutput/SumOf5Numbers/SumOf5.cs
@@ -0,0 +1,28 @@
+﻿//Problem 7. Sum of 5 Numbers
+
+//Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+//Examples:
+
+//numbers	            sum
+//1 2 3 4 5	            15
+//10 10 10 10 10	    50
+//1.5 3.14 8.2 -1 0	    11.84
+
+using System;
+
+class SumOf5
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter 5 numbers (given in a single line, separated by a space): ");
+        string[] numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        double sum = 0.0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += double.Parse(numbers[i]);
+        }
+
+        Console.WriteLine("Their sum is {0}", sum);
+    }
+}
