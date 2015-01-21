040300504747c31d0e1d4499b61dfb3b1b728ca3
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-21 15:04:13 +0200

    Added Problem 9. Sum of n Numbers

---
+++ CSharpPart-1/Homeworks/ConsoleInputOutput/SumOfNNumbers/SumOfN.cs
@@ -0,0 +1,38 @@
+﻿//Problem 9. Sum of n Numbers
+
+//Write a program that enters a number n and after that enters more n numbers and calculates and prints their sum. 
+//Note: You may need to use a for-loop.
+
+using System;
+
+class SumOfN
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int n = FindNumber();
+        double sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("Number {0} is: ", i);
+            sum += double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Their sum is {0}", sum);
+    }
+
+    private static int FindNumber()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        while (number < 0)
+        {
+            Console.Write("Invalid number!Number must be positive! Try again: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        return number;
+    }
+}
+
