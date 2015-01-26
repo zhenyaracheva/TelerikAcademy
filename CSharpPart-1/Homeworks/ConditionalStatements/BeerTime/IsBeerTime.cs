e9a50ac23e1ff4476ec5e0ae97d0a8f7981434f6
zhenyaracheva <zhenia.racheva@gmail.com>
2015-01-26 16:08:49 +0200

    Added Problem 10.* Beer Time

---
+++ CSharpPart-1/Homeworks/ConditionalStatements/BeerTime/IsBeerTime.cs
@@ -0,0 +1,40 @@
+﻿//Problem 10.* Beer Time
+
+//A beer time is after 1:00 PM and before 3:00 AM.
+//Write a program that enters a time in format “hh:mm tt” (an hour in range [01...12], a minute in range [00…59] and AM / PM designator) 
+//and prints beer time or non-beer time according to the definition above or invalid time if the time cannot be parsed. 
+//Note: You may need to learn how to parse dates and times.
+//Examples:
+
+//time	        result
+//1:00 PM	    beer time
+//4:30 PM	    beer time
+//10:57 PM	    beer time
+//8:30 AM	    non-beer time
+//02:59 AM	    beer time
+//03:00 AM	    non-beer time
+//03:26 AM	    non-beer time
+
+using System;
+
+class IsBeerTime
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a time in format “hh:mm tt” (an hour in range [01...12], a minute in range [00…59] and AM / PM designator)");
+        DateTime time = DateTime.Parse(Console.ReadLine());
+        DateTime startBeerTime = DateTime.Parse("1:00 PM");
+        DateTime endBeerTime = DateTime.Parse("3:00 AM");
+
+        if (time >= startBeerTime || time < endBeerTime)
+        {
+            Console.WriteLine("Beer time");
+        }
+        else
+        {
+            Console.WriteLine("Non-beer time!");
+        }
+
+    }
+}
+
