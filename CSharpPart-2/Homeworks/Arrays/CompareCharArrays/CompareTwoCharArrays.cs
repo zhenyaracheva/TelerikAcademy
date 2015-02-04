1c39357407f581c798a6b9c858cb90980cc21016
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-04 19:31:55 +0200

    Added Problem 3. Compare char arrays

--- CSharpPart-2/Homeworks/Arrays/CompareCharArrays/CompareTwoCharArrays.cs
+++ CSharpPart-2/Homeworks/Arrays/CompareCharArrays/CompareTwoCharArrays.cs
@@ -3,6 +3,7 @@
 //Write a program that compares two char arrays lexicographically (letter by letter).
 
 using System;
+using System.Linq;
 
 class CompareTwoCharArrays
 {
@@ -10,20 +11,46 @@ class CompareTwoCharArrays
     {
         Console.Write("Please, enter a lenght for the first array: ");
         int firstArrayLenght = int.Parse(Console.ReadLine());
-        char[] firstArray = new char[firstArrayLenght];
-
-        for (int i = 0; i < firstArrayLenght; i++)
-        {
-            Console.Write("Arra");
-        }
-
-
-
-
+        char[] firstArray = FillArray(firstArrayLenght);
+        
         Console.Write("Please, enter a lenght for the first array: ");
         int secondArrayLenght = int.Parse(Console.ReadLine());
-        char[] secondArray = new char[secondArrayLenght];
+        char[] secondArray = FillArray(secondArrayLenght);
 
+
+        if (firstArrayLenght == secondArrayLenght)
+        {
+            Array.Sort(firstArray);
+            Array.Sort(secondArray);
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine("Not equal!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Equal!");
+        }
+        else
+        {
+            Console.WriteLine("Not equal!");
+        }
+
+    }
+
+    private static char[] FillArray(int arrayLenght)
+    {
+        char[] array = new char[arrayLenght];
+        for (int i = 0; i < arrayLenght; i++)
+        {
+            Console.Write("First array[{0}] = ", i);
+            array[i] = char.Parse(Console.ReadLine());
+        }
+
+        return array;
     }
 }
 
