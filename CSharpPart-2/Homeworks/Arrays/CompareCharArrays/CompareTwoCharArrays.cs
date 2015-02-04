631d58de4d7e6ec517c54e723b766406d094b339
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-04 19:03:55 +0200

    Added Problem 2. Compare arrays

---
+++ CSharpPart-2/Homeworks/Arrays/CompareCharArrays/CompareTwoCharArrays.cs
@@ -0,0 +1,15 @@
+﻿//Problem 3. Compare char arrays
+
+//Write a program that compares two char arrays lexicographically (letter by letter).
+
+using System;
+
+class CompareTwoCharArrays
+{
+    static void Main()
+    {
+        Console.Write("Please, enter a lenght of");
+
+    }
+}
+
