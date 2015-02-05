e80d118b307edb34570d7b7f785911ab01102394
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-05 18:37:04 +0200

    Added Problem 7. Selection sort

---
+++ CSharpPart-2/Homeworks/Arrays/MaximalSum/FindMaximalSum.cs
@@ -0,0 +1,16 @@
+﻿//Problem 8. Maximal sum
+
+//Write a program that finds the sequence of maximal sum in given array.
+//Example:
+
+//            input	                          result
+//2, 3, -6, -1, 2, -1, 6, 4, -8, 8	        2, -1, 6, 4
+
+using System;
+
+class FindMaximalSum
+{
+    static void Main()
+    {
+    }
+}
