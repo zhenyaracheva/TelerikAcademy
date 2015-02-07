08bc2d24149b2318206c7c0ae91e07eb026d7e79
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-07 17:15:20 +0200

    Added Problem 19.* Permutations of set

---
+++ CSharpPart-2/Homeworks/Arrays/PermutationsOfSet/PrintPermutationsOfSet.cs
@@ -0,0 +1,78 @@
+﻿//Problem 19.* Permutations of set
+
+//Write a program that reads a number N and generates and prints all the permutations of the numbers [1 … N].
+//Example:
+
+//N	     result
+//3	    {1, 2, 3} 
+//      {1, 3, 2} 
+//      {2, 1, 3} 
+//      {2, 3, 1} 
+//      {3, 1, 2} 
+//      {3, 2, 1}
+
+using System;
+
+class PrintPermutationsOfSet
+{
+    static void Main()
+    {
+
+        Console.Write("Please, enter a number: ");
+        int n = CheckValidN();
+        int[] arrayOfNumbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arrayOfNumbers[i] = i + 1;
+        }
+
+        GeneratePermutations(arrayOfNumbers, 0);
+
+    }
+
+    private static int CheckValidN()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        while (n <= 0)
+        {
+            Console.Write("Number must be bigger tha 0! Try again: ");
+            n = int.Parse(Console.ReadLine());
+        }
+
+        return n;
+    }
+
+    static void GeneratePermutations<T>(T[] arr, int k)
+    {
+        if (k >= arr.Length)
+        {
+            Print(arr);
+        }
+        else
+        {
+            GeneratePermutations(arr, k + 1);
+            for (int i = k + 1; i < arr.Length; i++)
+            {
+                Swap(ref arr[k], ref arr[i]);
+                GeneratePermutations(arr, k + 1);
+                Swap(ref arr[k], ref arr[i]);
+            }
+        }
+    }
+
+    static void Print<T>(T[] arr)
+    {
+        Console.WriteLine(string.Join(", ", arr));
+    }
+
+    static void Swap<T>(ref T first, ref T second)
+    {
+        T oldFirst = first;
+        first = second;
+        second = oldFirst;
+    }
+
+}
+
---
+++ CSharpPart-2/Homeworks/Arrays/RemoveElementsFromArray/RemoveElements.cs
@@ -0,0 +1,106 @@
+﻿//Problem 18.* Remove elements from array
+
+//Write a program that reads an array of integers and removes from it a minimal number of elements in such a way that the 
+//remaining array is sorted in increasing order.
+//Print the remaining sorted array.
+//Example:
+
+//          input	               result
+//6, 1, 4, 3, 0, 3, 6, 4, 5	    1, 3, 3, 4, 5
+
+using System;
+using System.Collections.Generic;
+
+class RemoveElements
+{
+    static List<int> maxSolution = new List<int>();
+
+    static void Main()
+    {
+        Console.WriteLine("Please, enter numbers of array (separated by space or \",\"): ");
+        string[] inputArray = CheckIsValidInput();
+        int[] arrayOfNumbers = CreateArray(inputArray);
+
+        for (int i = 0; i < arrayOfNumbers.Length; i++)
+        {
+            FindCombination(i, arrayOfNumbers, new List<int>());
+        }
+
+        Console.Write("Solution: ");
+        if (arrayOfNumbers.Length == 1)
+        {
+            Console.Write(arrayOfNumbers[0]);
+        }
+        else
+        {
+            for (int i = 0; i < maxSolution.Count; i++)
+            {
+                Console.Write("{0} ", maxSolution[i]);
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void FindCombination(int index, int[] numbers, List<int> currentSolution)
+    {
+        if (index >= numbers.Length)
+        {
+            if (currentSolution.Count > maxSolution.Count)
+            {
+                maxSolution.Clear();
+
+                for (int j = 0; j < currentSolution.Count; j++)
+                {
+                    maxSolution.Add(currentSolution[j]);
+                }
+            }
+
+            return;
+        }
+
+        for (int i = index; i < numbers.Length; i++)
+        {
+            if (currentSolution.Count == 0)
+            {
+                currentSolution.Add(numbers[i]);
+            }
+            else
+            {
+                if (numbers[i] >= currentSolution[currentSolution.Count - 1])
+                {
+                    currentSolution.Add(numbers[i]);
+                    FindCombination(i + 1, numbers, currentSolution);
+                    currentSolution.RemoveAt(currentSolution.Count - 1);
+                }
+            }
+        }
+
+    }
+
+    private static string[] CheckIsValidInput()
+    {
+        string[] input = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        while (input.Length <= 0)
+        {
+            Console.WriteLine("Empty array! Try again!");
+            input = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        return input;
+    }
+
+    private static int[] CreateArray(string[] arrayOfStrings)
+    {
+        int[] arrayOfNumbers = new int[arrayOfStrings.Length];
+
+        for (int i = 0; i < arrayOfNumbers.Length; i++)
+        {
+            arrayOfNumbers[i] = int.Parse(arrayOfStrings[i]);
+        }
+
+        return arrayOfNumbers;
+    }
+}
+
