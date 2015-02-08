0e577be0932878dd6571190d8693752cd371438e
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-08 22:43:47 +0200

    Added Problem 2. Get largest number

---
+++ CSharpPart-2/Homeworks/Methods/GetLargestNumber/PrintLargestNumber.cs
@@ -0,0 +1,29 @@
+﻿//Problem 2. Get largest number
+
+//Write a method GetMax() with two parameters that returns the larger of two integers.
+//Write a program that reads 3 integers from the console and prints the largest of them using the method GetMax().
+
+using System;
+
+class PrintLargestNumber
+{
+    static void Main()
+    {
+        Console.Write("Please, enter 3 integers (separate by space): ");
+        string[] numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int max = int.Parse(numbers[0]);
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            max = GetMax(max, int.Parse(numbers[i])); 
+        }
+
+        Console.WriteLine("Max number is {0}", max);
+    }
+    
+    static int GetMax(int first, int second)
+    {
+        return first > second ? first : second;
+    }
+}
+
