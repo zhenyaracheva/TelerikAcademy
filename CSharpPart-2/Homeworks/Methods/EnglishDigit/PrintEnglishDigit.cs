d97c09a3268caaa6edf146921b15a8d50f3c3f51
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-08 22:56:48 +0200

    Added Problem 3. English digit

---
+++ CSharpPart-2/Homeworks/Methods/EnglishDigit/PrintEnglishDigit.cs
@@ -0,0 +1,42 @@
+﻿//Problem 3. English digit
+
+//Write a method that returns the last digit of given integer as an English word.
+//Examples:
+
+//input	    output
+//512	    two
+//1024	    four
+//12309	    nine
+
+using System;
+
+class PrintEnglishDigit
+{
+    static void Main()
+    {
+        Console.Write("Please, enter an integer: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Last digit as is {0}!", GetLastGigitAsWord(number));
+    }
+
+    private static string GetLastGigitAsWord(int number)
+    {
+        int lastDigit = number % 10;
+
+        switch (lastDigit)
+        {
+            case 0: return "zero";
+            case 1: return "one";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+            default: return "Invalid number!";
+        }
+    }
+}
+
