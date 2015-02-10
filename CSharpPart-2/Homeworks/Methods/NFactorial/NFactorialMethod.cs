a77eaffbce51cb6614169e1e5abb5a1c8842259a
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-10 10:32:21 +0200

    Added Problem 10. N Factorial

---
+++ CSharpPart-2/Homeworks/Methods/NFactorial/NFactorialMethod.cs
@@ -0,0 +1,32 @@
+﻿//Problem 10. N Factorial
+
+//Write a program to calculate n! for each n in the range [1..100].
+//Hint: Implement first a method that multiplies a number represented as array of digits by given integer number
+
+using System;
+using System.Linq;
+using System.Numerics;
+
+class NFactorialMethod
+{
+    static void Main()
+    {
+        Console.Write("Please, enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        BigInteger factorial = Factorial(number);
+        Console.WriteLine("{0}! = {1}", number, factorial);
+    }
+
+    private static BigInteger Factorial(int number)
+    {
+        BigInteger result = 1;
+
+        for (int i = 2; i <= number; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
+
