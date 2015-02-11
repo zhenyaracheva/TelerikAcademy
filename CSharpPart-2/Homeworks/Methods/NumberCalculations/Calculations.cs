13776c0ef44837edcd707dd22e6bf8cacc287588
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-11 21:43:32 +0200

    Added Problem 15.* Number calculations

--- CSharpPart-2/Homeworks/Methods/NumberCalculations/Calculations.cs
+++ CSharpPart-2/Homeworks/Methods/NumberCalculations/Calculations.cs
@@ -10,10 +10,21 @@ class Calculations
 {
     static void Main()
     {
-        Console.WriteLine("Please, enter an array of numbers (separate by space):");
-        dynamic[] numbers = CreateArrayOfNumbers();
-
-        Console.WriteLine("Min number: {0}", MinValue(numbers));
+        Console.WriteLine("Methods with no floating point numbers:");
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("Min value: {0}", MinValue(1, 2, 3));
+        Console.WriteLine("Max value: {0}", MaxValue(1, 2, 3));
+        Console.WriteLine("Average: {0}", AverageValue(1, 2, 3));
+        Console.WriteLine("Sum: {0}", Sum(1, 2, 3));
+        Console.WriteLine("Product: {0}", Product(1, 2, 3));
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("Methods with floating piont numbers:");
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("Min value: {0}", MinValue(1.2, 2.2, 3.2));
+        Console.WriteLine("Max value: {0}", MaxValue(1.2, 2.2, 3.2));
+        Console.WriteLine("Average: {0}", AverageValue(1.2, 2.2, 3.2));
+        Console.WriteLine("Sum: {0}", Sum(1.2, 2.2, 3.2));
+        Console.WriteLine("Product: {0}", Product(1.2, 2.2, 3.2));
     }
 
     private static T Product<T>(params T[] numbers)
@@ -22,7 +33,7 @@ class Calculations
 
         for (int i = 0; i < numbers.Length; i++)
         {
-            product *= numbers[i];
+            product *= (dynamic)numbers[i];
         }
 
         return product;
@@ -32,9 +43,10 @@ class Calculations
     {
         dynamic sum = 0;
 
+
         for (int i = 0; i < numbers.Length; i++)
         {
-            sum += numbers[i];
+            sum += (dynamic)numbers[i];
         }
 
         return sum;
@@ -46,7 +58,7 @@ class Calculations
 
         for (int i = 0; i < numbers.Length; i++)
         {
-            average += numbers[i];
+            average += (dynamic)numbers[i];
         }
 
         return average / numbers.Length;
@@ -60,19 +72,6 @@ class Calculations
     private static T MinValue<T>(params T[] numbers)
     {
         return numbers.Min();
-    }
-
-    private static string[] CreateArrayOfNumbers()
-    {
-        string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        while (input.Length <= 0)
-        {
-            Console.Write("Empty array! Try again: ");
-            input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        return input;
     }
 }
 
