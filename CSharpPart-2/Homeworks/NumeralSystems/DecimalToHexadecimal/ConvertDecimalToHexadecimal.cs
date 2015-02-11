14a81194a758d84ee3b0d266bb3ffd2a7626a5d1
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-11 22:27:12 +0200

    Added Problem 3. Decimal to hexadecimal

---
+++ CSharpPart-2/Homeworks/NumeralSystems/DecimalToHexadecimal/ConvertDecimalToHexadecimal.cs
@@ -0,0 +1,16 @@
+﻿//Problem 3. Decimal to hexadecimal
+
+//Write a program to convert decimal numbers to their hexadecimal representation
+
+using System;
+using System.Linq;
+class ConvertDecimalToHexadecimal
+{
+    static void Main()
+    {
+        Console.Write("Please, enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Hexadecimal representation: {0}",number.ToString("X"));
+    }
+}
+
