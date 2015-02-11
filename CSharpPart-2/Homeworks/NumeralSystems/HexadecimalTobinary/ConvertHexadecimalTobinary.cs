6b7693a9a03a4e820d0e2f2d9cfb22e228536035
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-11 23:11:34 +0200

    Added Problem 5. Hexadecimal to binary

---
+++ CSharpPart-2/Homeworks/NumeralSystems/HexadecimalTobinary/ConvertHexadecimalTobinary.cs
@@ -0,0 +1,17 @@
+﻿//Problem 5. Hexadecimal to binary
+
+//Write a program to convert hexadecimal numbers to binary numbers (directly).
+
+using System;
+using System.Linq;
+
+class ConvertHexadecimalTobinary
+{
+    static void Main()
+    {
+        Console.Write("Please, enter hexademical number: ");
+        string hex = Console.ReadLine();
+        Console.WriteLine("Binary: {0}",Convert.ToString(Convert.ToInt64(hex, 16), 2));
+    }
+}
+
