ccda7b8f23eeada6509033143d04cb11f6a9ca24
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-11 22:03:49 +0200

    Added Problem 1. Decimal to binary

---
+++ CSharpPart-2/Homeworks/NumeralSystems/DecimalToBinary/ConvertDecimalToBinary.cs
@@ -0,0 +1,18 @@
+﻿//Problem 1. Decimal to binary
+
+//Write a program to convert decimal numbers to their binary representation.
+
+using System;
+using System.Linq;
+
+class ConvertDecimalToBinary
+{
+    static void Main()
+    {
+        Console.Write("Please, enter number: ");
+        long num = long.Parse(Console.ReadLine());
+        string binary = Convert.ToString(num, 2);
+        Console.WriteLine(binary);
+    }
+}
+
