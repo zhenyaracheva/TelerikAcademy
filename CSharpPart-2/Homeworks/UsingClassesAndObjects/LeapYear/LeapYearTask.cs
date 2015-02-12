6e960cc6913c95273577d5ada385f9cfd77cf233
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-12 21:43:57 +0200

    Added Problem 1. Leap year

---
+++ CSharpPart-2/Homeworks/UsingClassesAndObjects/LeapYear/LeapYearTask.cs
@@ -0,0 +1,17 @@
+﻿//Problem 1. Leap year
+
+//Write a program that reads a year from the console and checks whether it is a leap one.
+//Use System.DateTime.
+
+using System;
+
+class LeapYearTask
+{
+    static void Main()
+    {
+        Console.Write("Please, enter an year: ");
+        DateTime currerntYear = new DateTime(int.Parse(Console.ReadLine()), 2, 1);
+        Console.WriteLine("Is {0} leap year: {1}!", currerntYear.Year, DateTime.IsLeapYear(currerntYear.Year));
+    }
+}
+
