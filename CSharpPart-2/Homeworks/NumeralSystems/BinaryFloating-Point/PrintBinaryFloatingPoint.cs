12cb36f72a81161e5e2cff4d1f9d01cf943d7f18
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-12 16:24:25 +0200

    Added Problem 9.* Binary floating-point

---
+++ CSharpPart-2/Homeworks/NumeralSystems/BinaryFloating-Point/PrintBinaryFloatingPoint.cs
@@ -0,0 +1,38 @@
+﻿//Problem 9.* Binary floating-point
+
+//Write a program that shows the internal binary representation of given 32-bit signed 
+//floating-point number in IEEE 754 format (the C# type float).
+//Example:
+
+//number	sign	exponent	            mantissa
+//-27,25	  1	    10000011	     10110100000000000000000
+
+using System;
+using System.Linq;
+
+class PrintBinaryFloatingPoint
+{
+    static void Main()
+    {
+        Console.Write("Please, enter a floating-point number: ");
+        float number = float.Parse(Console.ReadLine());
+        string binary = ConvertFloatingPointToBynary(number);
+        Console.WriteLine("Binary: {0}", binary);
+        Console.WriteLine("Sign: {0}", binary[0]);
+        Console.WriteLine("Exponent: {0}", binary.Substring(1, 8));
+        Console.WriteLine("Mantissa: {0}", binary.Substring(9, binary.Length - 9));
+    }
+
+    private static string ConvertFloatingPointToBynary(float number)
+    {
+        if (number == 0)
+        {
+            return Convert.ToString(0, 2).PadLeft(32, '0');
+        }
+
+        char sign = number < 0 ? '1' : '0';
+        number = Math.Abs(number);
+        int temp = BitConverter.ToInt32(BitConverter.GetBytes(number), 0);
+        return sign + Convert.ToString(temp, 2);
+    }
+}
