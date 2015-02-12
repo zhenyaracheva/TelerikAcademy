56e257fa35563e45ccca3d7c5ba69e7ac6a2ea11
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-12 21:51:05 +0200

    Added Problem 2. Random numbers

---
+++ CSharpPart-2/Homeworks/UsingClassesAndObjects/RandomNumbers/PrintRandomNumbers.cs
@@ -0,0 +1,21 @@
+﻿//Problem 2. Random numbers
+
+//Write a program that generates and prints to the console 10 random values in the range [100, 200].
+
+using System;
+
+class PrintRandomNumbers
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("{0} ", rand.Next(100, 201));
+        }
+
+        Console.WriteLine();
+    }
+}
+
