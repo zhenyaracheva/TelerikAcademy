55f10e4a9f59b9c95715359b2f88a66da104ca15
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-16 22:43:10 +0200

    Added Problem 5. Parse tags

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/ParseTags/Tags.cs
@@ -0,0 +1,53 @@
+﻿//Problem 5. Parse tags
+
+//You are given a text. Write a program that changes the text in all regions surrounded by the tags <upcase> and </upcase> to upper-case.
+//The tags cannot be nested.
+//Example: We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.
+//The expected result: We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
+
+using System;
+using System.Text;
+class Tags
+{
+    static void Main()
+    {
+        Console.WriteLine("Please,enter text: ");
+        string text = Console.ReadLine();
+        StringBuilder converted = new StringBuilder();
+        string startTag = "<upcase>";
+        string endTag = "</upcase>";
+        bool inTag = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '<')
+            {
+                if (text.Substring(i, startTag.Length) == startTag)
+                {
+                    inTag = true;
+                    i += startTag.Length - 1;
+                }
+                else if (text.Substring(i, endTag.Length) == endTag)
+                {
+                    inTag = false;
+                    i += endTag.Length - 1;
+                }
+                else
+                {
+                    converted.Append(text[i]);
+                }
+            }
+            else if (char.IsDigit(text[i]) || inTag)
+            {
+                converted.Append(text[i].ToString().ToUpper());
+            }
+            else if (char.IsDigit(text[i]) || !inTag)
+            {
+                converted.Append(text[i]);
+            }
+        }
+
+        Console.WriteLine(converted);
+    }
+}
+
