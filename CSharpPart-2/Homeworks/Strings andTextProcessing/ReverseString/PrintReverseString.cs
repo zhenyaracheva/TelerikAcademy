9cd1138d843b6340ad0de8162dd415b16026e250
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-16 21:15:12 +0200

    Added Problem 2. Reverse string

---
+++ CSharpPart-2/Homeworks/Strings andTextProcessing/ReverseString/PrintReverseString.cs
@@ -0,0 +1,22 @@
+﻿//Problem 2. Reverse string
+
+//Write a program that reads a string, reverses it and prints the result at the console.
+//Example:
+
+//input	    output
+//sample	elpmas
+
+using System;
+using System.Linq;
+
+class PrintReverseString
+{
+    static void Main()
+    {
+        Console.Write("Please,enter a word: ");
+        string word = Console.ReadLine();
+        Console.Write("Reversed: ");
+        Console.WriteLine(string.Concat(Enumerable.Reverse(word)));
+    }
+}
+
