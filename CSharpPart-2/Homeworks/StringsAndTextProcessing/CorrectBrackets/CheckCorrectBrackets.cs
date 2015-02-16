c314396e0c952628bb154c037da14443e1fc1498
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-16 21:28:47 +0200

    Added Problem 3. Correct brackets

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/CorrectBrackets/CheckCorrectBrackets.cs
@@ -0,0 +1,44 @@
+﻿//Problem 3. Correct brackets
+
+//Write a program to check if in a given expression the brackets are put correctly.
+//Example of correct expression: ((a+b)/5-d). Example of incorrect expression: )(a+b)).
+
+using System;
+using System.Collections.Generic;
+class CheckCorrectBrackets
+{
+    static void Main()
+    {
+        Console.Write("Please, enter an expression: ");
+        string expression = Console.ReadLine();
+        Stack<char> brackets = new Stack<char>();
+
+        for (int i = 0; i < expression.Length; i++)
+        {
+            if(expression[i]=='(')
+            {
+                brackets.Push('(');
+            }
+            else if (expression[i]==')')
+            {
+                if(brackets.Count==0)
+                {
+                    Console.WriteLine("Invalid brackets!");
+                    return;
+                }
+
+                brackets.Pop();
+            }
+        }
+
+        if (brackets.Count!=0)
+        {
+            Console.WriteLine("Invalid brackets!");
+        }
+        else
+        {
+            Console.WriteLine("Correct brackets!");
+        }
+    }
+}
+
