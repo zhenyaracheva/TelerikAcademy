2c172267a68ac350a2a88dadbe661a913596ce69
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-17 18:22:53 +0200

    Added Problem 15. Replace tags

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/ReplaceTags/TagReplace.cs
@@ -0,0 +1,21 @@
+﻿//Problem 15. Replace tags
+
+//Write a program that replaces in a HTML document given as string all the tags <a href="…">…</a> with corresponding tags [URL=…]…/URL].
+
+using System;
+using System.Text.RegularExpressions;
+
+class TagReplace
+{
+    static void Main()
+    {
+        Console.WriteLine("Please, enter a tag <a href=\"…\">…</a>: ");
+        string tag = Console.ReadLine();
+        string patternOriginal = @"<a href=""(.*?)"">(.*?)</a>";
+        string patternReplace = @"[URL=$1]$2[/URL]";
+
+        var match = Regex.Replace(tag, patternOriginal, patternReplace);
+        Console.WriteLine(match);
+    }
+}
+
