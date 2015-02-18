d8c8985e1b4cf398fc77fa3235258874058ff9eb
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-18 22:06:27 +0200

    Added Problem 17. Date in Bulgarian

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/DateInBulgarian/PrintDateInBulgarian.cs
@@ -0,0 +1,21 @@
+﻿//Problem 17. Date in Bulgarian
+
+//Write a program that reads a date and time given in the format: day.month.year hour:minute:second 
+//and prints the date and time after 6 hours and 30 minutes (in the same format) along with the day of week in Bulgarian.
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+class PrintDateInBulgarian
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Please,enter a date and time given in the format:day.month.year hour:minute:second");
+        DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy HH:mm:ss", new CultureInfo("bg-BG")).AddHours(6).AddMinutes(30);
+        Console.WriteLine("{0} {1}", date.ToString("dddd", new CultureInfo("bg-BG")), date);
+    }
+}
+
