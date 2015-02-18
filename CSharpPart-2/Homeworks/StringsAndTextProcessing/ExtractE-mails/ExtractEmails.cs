ce095684f29b982ec7373d566cf3d9174654192b
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-18 22:26:46 +0200

    Added Extract e-mails

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/ExtractE-mails/ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿//Problem 18. Extract e-mails
+
+//Write a program for extracting all email addresses from given text.
+//All sub-strings that match the format <identifier>@<host>…<domain> should be recognized as emails.
+
+using System;
+using System.Text.RegularExpressions;
+
+class ExtractEmails
+{
+    static void Main()
+    {
+        string RegexPattern = @"\b[A-Z0-9._-]+@[A-Z0-9][A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z.]{2,6}\b";
+        string str = Console.ReadLine();
+        var matches = Regex.Matches(str, RegexPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+        for (int i = 0; i < matches.Count; i++)
+        {
+            Console.WriteLine(matches[i]);
+        }
+    }
+}
