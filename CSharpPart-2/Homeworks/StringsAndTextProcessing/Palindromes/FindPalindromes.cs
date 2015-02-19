6de695301e30e398fb4005c823e38b79f99d140a
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-19 16:04:39 +0200

    Added Problem 20. Palindromes

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/Palindromes/FindPalindromes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class FindPalindromes
+{
+    static void Main()
+    {
+        Console.Write("Please, enter text: ");
+        string text = Console.ReadLine();
+        var matches = Regex.Matches(text, @"\b\w+\b");
+
+        foreach (var match in matches)
+        {
+            if (IsPalindrom(match.ToString()))
+            {
+                Console.WriteLine(match);
+
+            }
+        }
+    }
+
+    public static bool IsPalindrom(string str)
+    {
+        int i = 0;
+        int j = str.Length - 1;
+
+        while (i < j)
+        {
+            if (str[i] != str[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
+
