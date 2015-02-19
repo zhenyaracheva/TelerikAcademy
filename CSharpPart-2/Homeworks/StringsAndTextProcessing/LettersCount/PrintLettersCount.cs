5266e21e3398291be36c7003625d94f4195bb1e6
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-19 16:22:50 +0200

    Added Problem 21. Letters count

--- CSharpPart-2/Homeworks/StringsAndTextProcessing/LettersCount/PrintLettersCount.cs
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/LettersCount/PrintLettersCount.cs
@@ -4,13 +4,26 @@
 //in the string along with information how many times each letter is found.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class PrintLettersCount
 {
     static void Main()
     {
-        Console.Write("Please, enter ");
+        Console.Write("Please, enter text: ");
+        string text = Console.ReadLine();
+        HashSet<char> chars = new HashSet<char>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                chars.Add(text[i]);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", chars.OrderBy(x => x)));
     }
 }
 
