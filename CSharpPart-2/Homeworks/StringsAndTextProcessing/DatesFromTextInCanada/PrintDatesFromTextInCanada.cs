596942b62d1fcffa2bc49334f4681fe68812abe9
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-19 15:53:24 +0200

    Added Problem 19. Dates from text in Canada

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/DatesFromTextInCanada/PrintDatesFromTextInCanada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+class PrintDatesFromTextInCanada
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-CA");
+        Console.WriteLine("Please,enter a text:");
+        string text = Console.ReadLine();
+        string format = @"\b\d{2}.\d{2}.\d{4}\b";
+
+        DateTime currentDate;
+        foreach (Match match in Regex.Matches(text, format))
+        {
+
+            if (DateTime.TryParseExact(match.Value, "d.M.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out currentDate))
+            {
+                Console.WriteLine(currentDate.ToShortDateString());
+            }
+        }
+    }
+}
+
