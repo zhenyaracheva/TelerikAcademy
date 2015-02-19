56f1660a65bcc7c6ec4ae7854fc94903e6d7c0a4
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-19 19:41:41 +0200

    Added Problem 24. Order words

--- CSharpPart-2/Homeworks/StringsAndTextProcessing/OrderWords/PrintOrderWords.cs
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/OrderWords/PrintOrderWords.cs
@@ -3,6 +3,8 @@
 //Write a program that reads a list of words, separated by spaces and prints the list in an alphabetical order.
 
 using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Linq;
 
 class PrintOrderWords
@@ -10,6 +12,8 @@ class PrintOrderWords
     static void Main()
     {
         Console.Write("Please, enter a list of words, separate by space: ");
+        var words = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList().OrderBy(x => x);
+        Console.WriteLine(string.Join(", ", words));
     }
 }
 
