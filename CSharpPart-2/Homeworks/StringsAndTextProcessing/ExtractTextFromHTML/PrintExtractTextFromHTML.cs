baff84ba58d8b44b553778a68f6297002cb5b9a4
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-19 20:20:54 +0200

    Added Problem 25. Extract text from HTML

---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/ExtractTextFromHTML/PrintExtractTextFromHTML.cs
@@ -0,0 +1,63 @@
+﻿//Problem 25. Extract text from HTML
+
+//Write a program that extracts from given HTML file its title (if available), and its body text without the HTML tags.
+//Example input:
+
+//<html>
+//  <head><title>News</title></head>
+//  <body><p><a href="http://academy.telerik.com">Telerik
+//    Academy</a>aims to provide free real-world practical
+//    training for young people who want to turn into
+//    skilful .NET software engineers.</p></body>
+//</html>
+//Output:
+
+//Title: News
+
+//Text: Telerik Academy aims to provide free real-world practical training for young people who want to 
+//turn into skilful .NET software engineers.
+
+using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+
+class PrintExtractTextFromHTML
+{
+    static void Main()
+    {
+        string html = "<html><head><title>News</title></head><body><p><a href=\"http://academy.telerik.com\">Telerik Academy</a>aims to provide free real-world practical training for young people who want to turn into skilful .NET software engineers.</p></body></html>";
+
+        int count = 1;
+        if (html.Contains("title"))
+        {
+            count = 0;
+        }
+        var matches = Regex.Matches(html, @"(?<=^|>)[^><]+?(?=<|$)");
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (var match in matches)
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Title: {0}", match);
+                Console.WriteLine();
+                count++;
+            }
+            else if (count == 1)
+            {
+                result.AppendFormat("Text: {0} ", match);
+                count++;
+            }
+            else
+            {
+                result.AppendFormat("{0} ", match);
+            }
+        }
+
+        Console.WriteLine(result.ToString().Trim());
+    }
+}
+
---
+++ CSharpPart-2/Homeworks/StringsAndTextProcessing/ExtractTextFromHTML/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExtractTextFromHTML")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExtractTextFromHTML")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4224d535-6efb-47f4-a656-190960dc138d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
