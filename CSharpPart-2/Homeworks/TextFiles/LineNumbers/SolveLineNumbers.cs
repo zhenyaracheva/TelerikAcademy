f69e2491f9ca5845fc46b074e2f479ed5509241b
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-27 21:27:10 +0200

    Added Problem 3. Line numbers

---
+++ CSharpPart-2/Homeworks/TextFiles/LineNumbers/SolveLineNumbers.cs
@@ -0,0 +1,33 @@
+﻿//Problem 3. Line numbers
+
+//Write a program that reads a text file and inserts line numbers in front of each of its lines.
+//The result should be written to another text file.
+
+using System;
+using System.IO;
+
+class SolveLineNumbers
+{
+    static void Main()
+    {
+
+        using (StreamReader reader = new StreamReader(@"..\..\text.txt"))
+        {
+            using (StreamWriter writer = new StreamWriter(@"..\..\textWithLines.txt"))
+            {
+                int count = 1;
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    writer.WriteLine("{0}. {1}", count, line);
+                    line = reader.ReadLine();
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
+
