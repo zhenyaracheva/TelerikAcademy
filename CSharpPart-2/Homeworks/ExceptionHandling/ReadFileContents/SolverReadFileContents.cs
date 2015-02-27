a1eec34005582087de9fdb5b84d450a946e6826d
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-27 19:18:01 +0200

    Added Problem 3. Read file contents

---
+++ CSharpPart-2/Homeworks/ExceptionHandling/ReadFileContents/SolverReadFileContents.cs
@@ -0,0 +1,68 @@
+﻿//Problem 3. Read file contents
+
+//Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), 
+//reads its contents and prints it on the console.
+//Find in MSDN how to use System.IO.File.ReadAllText(…).
+//Be sure to catch all possible exceptions and print user-friendly error messages.
+
+using System;
+using System.IO;
+using System.Security;
+
+class SolverReadFileContents
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Please, enter file name along with its full file path: ");
+            string filePath = Console.ReadLine();
+            string fileContent = ReadFileContents(filePath);
+            Console.WriteLine(fileContent);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("The file specified in path was not found.");
+        }
+
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Path is null.");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Path is a zero-length string, contains only white space, or contains one or more invalid characters.");
+        }
+        catch (PathTooLongException)
+        {
+            Console.WriteLine("The specified path, file name, or both exceed the system-defined maximum length.");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine("The specified path is invalid.");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("An I/O error occurred while opening the file.");
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("Path specified a file that is read-only, this operation is not supported on the current platform or you don't have the required permission.");
+        }
+        catch (SecurityException)
+        {
+            Console.WriteLine("You don't have the required permission.");
+        }
+        catch (NotSupportedException)
+        {
+            Console.WriteLine("Path is in an invalid format.");
+        }
+
+    }
+
+    private static string ReadFileContents(string filePath)
+    {
+        return File.ReadAllText(filePath);
+    }
+}
+
