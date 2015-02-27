a245b95f0632653a93b4ad00d556ed9f9949c494
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-27 21:06:42 +0200

    Added Problem 2. Concatenate text files

---
+++ CSharpPart-2/Homeworks/TextFiles/ConcatenateTextFiles/SolveConcatenateTextFiles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class SolveConcatenateTextFiles
+{
+    static void Main()
+    {
+
+        StreamWriter writer = new StreamWriter("thirdText.txt");
+
+        using (writer)
+        {
+            using (StreamReader firstReader = new StreamReader("firstText.txt"))
+            {
+                string line = firstReader.ReadLine();
+
+                while (line != null)
+                {
+                    writer.WriteLine(line);
+                    line = firstReader.ReadLine();
+                }
+            }
+
+            using (StreamReader secondReader = new StreamReader("secondText.txt"))
+            {
+                string line = secondReader.ReadLine();
+
+                while (line != null)
+                {
+                    writer.WriteLine(line);
+                    line = secondReader.ReadLine();
+                }
+            }
+        }
+
+        Console.WriteLine("Concatenated text files is done!");
+    }
+}
