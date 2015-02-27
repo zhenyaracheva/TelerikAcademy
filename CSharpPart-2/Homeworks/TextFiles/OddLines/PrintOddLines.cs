b490ddfff8e7cce7fea7ce3460c9c80a67954159
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-27 20:37:24 +0200

    Added Problem 1. Odd lines

---
+++ CSharpPart-2/Homeworks/TextFiles/OddLines/PrintOddLines.cs
@@ -0,0 +1,33 @@
+﻿//Problem 1. Odd lines
+
+//Write a program that reads a text file and prints on the console its odd lines
+
+using System;
+using System.IO;
+
+class PrintOddLines
+{
+    static void Main()
+    {
+        StreamReader reader = new StreamReader("text.txt");
+
+        using (reader)
+        {
+            int lineCount = 0;
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+
+                if (lineCount % 2 == 1)
+                {
+                    Console.WriteLine(line);
+                }
+
+                line = reader.ReadLine();
+                lineCount++;
+            }
+        }
+    }
+}
+
