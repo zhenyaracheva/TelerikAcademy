a7ed9e6f2e2e86a17399e61263817e564202266e
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-28 12:22:23 +0200

    Added Problem 8. Replace whole word

---
+++ CSharpPart-2/Homeworks/TextFiles/ReplaceWholeWord/SolveReplaceWholeWord.cs
@@ -0,0 +1,36 @@
+﻿//Problem 8. Replace whole word
+
+//Modify the solution of the previous problem to replace only whole words (not strings).
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class SolveReplaceWholeWord
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(@"..\..\input.txt"))
+        {
+            using (StreamWriter writer = new StreamWriter(@"..\..\output.txt"))
+            {
+                string line = reader.ReadLine();
+                StringBuilder result = new StringBuilder();
+
+                while (line != null)
+                {
+                    var replaced = Regex.Replace(line, @"\bstart\b", "finish");
+                    result.AppendLine(replaced);
+                    line = reader.ReadLine();
+                }
+
+                writer.WriteLine(result);
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
+
