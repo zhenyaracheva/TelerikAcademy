1df1f6bba5c15aa603ba333bd75fe39f31943560
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-28 12:45:40 +0200

    Added Problem 9. Delete odd lines

---
+++ CSharpPart-2/Homeworks/TextFiles/DeleteOddLines/SolveDeleteOddLines.cs
@@ -0,0 +1,41 @@
+﻿//Problem 9. Delete odd lines
+
+//Write a program that deletes from given text file all odd lines.
+//The result should be in the same file.
+
+using System;
+using System.IO;
+using System.Text;
+
+class SolveDeleteOddLines
+{
+    static void Main()
+    {
+        StringBuilder result = new StringBuilder();
+
+        using (StreamReader reader = new StreamReader(@"..\..\text.txt"))
+        {
+            string line = reader.ReadLine();
+            int count = 1;
+
+            while (line != null)
+            {
+                if (count % 2 == 0)
+                {
+                    result.AppendLine(line);
+                }
+
+                line = string.Empty;
+                line = reader.ReadLine();
+                count++;
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter(@"..\..\text.txt"))
+        {
+            writer.WriteLine(result);
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
