b1ff208a2ef352979d41e4411a79b7e4ad2f781e
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-28 12:06:19 +0200

    Added Problem 7. Replace sub-string

--- CSharpPart-2/Homeworks/TextFiles/ReplaceSubString/SoleveReplaceSubString.cs
+++ CSharpPart-2/Homeworks/TextFiles/ReplaceSubString/SoleveReplaceSubString.cs
@@ -1,7 +1,7 @@
 ﻿//Problem 7. Replace sub-string
 
 //Write a program that replaces all occurrences of the sub-string start with the sub-string finish in a text file.
-//Ensure it will work with large files (e.g. 100 MB). 
+//Ensure it will work with large files (e.g. 100 MB).  
 
 using System;
 using System.Linq;
