9f6c0a78cb06213d47223ef2eddee857174c059a
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-28 13:58:45 +0200

    Added Problem 10. Extract text from XML

---
+++ CSharpPart-2/Homeworks/TextFiles/ExtractTextFromXML/SolveExtractTextFromXML.cs
@@ -0,0 +1,46 @@
+﻿//Problem 10. Extract text from XML
+
+//Write a program that extracts from given XML file all the text without the tags.
+//Example:
+//<?xml version="1.0"><student><name>Pesho</name><age>21</age><interests count="3"><interest>Games
+//</interest><interest>C#</interest><interest>Java</interest></interests></student>
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class SolveExtractTextFromXML
+{
+    static void Main()
+    {
+        StringBuilder result = new StringBuilder();
+
+        using (StreamReader reader = new StreamReader(@"..\..\input.txt"))
+        {
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                var matches = Regex.Matches(line, @">\w+<");
+                foreach (var match in matches)
+                {
+                    result.Append(match.ToString().Replace(">", string.Empty).Replace("<", string.Empty));
+                    result.Append(" ");
+                }
+
+                line = reader.ReadLine();
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter(@"..\..\output.txt"))
+        {
+            result.Length--;
+            writer.WriteLine(result);
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
+
