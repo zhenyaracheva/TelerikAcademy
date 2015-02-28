69707b9301d64dc187df0281cc62b5b2b05d2a7e
zhenyaracheva <zhenia.racheva@gmail.com>
2015-02-28 12:04:51 +0200

    Update

---
+++ CSharpPart-2/Homeworks/TextFiles/ReplaceSubString/SoleveReplaceSubString.cs
@@ -0,0 +1,35 @@
+﻿//Problem 7. Replace sub-string
+
+//Write a program that replaces all occurrences of the sub-string start with the sub-string finish in a text file.
+//Ensure it will work with large files (e.g. 100 MB). 
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+class SolveReplaceSubString
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(@"..\..\input.txt"))
+        {
+            using (StreamWriter writer = new StreamWriter(@"..\..\output.txt"))
+            {
+                string line = reader.ReadLine();
+                StringBuilder result = new StringBuilder();
+
+                while (line != null)
+                {
+                    result.AppendLine(line.Replace("start", "finish"));
+                    line = reader.ReadLine();
+                }
+
+                writer.WriteLine(result);
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
+
