2520c88c13d3d782636eb5bcc4d282f2ad7b179d
zhenyaracheva <zhenia.racheva@gmail.com>
2015-03-15 14:28:01 +0200

    Added Problem 11. Version attribute

---
+++ OOP/Homeworks/DefiningClassesPart2/VersionAttributeTask/AttributesMain.cs
@@ -0,0 +1,20 @@
+﻿namespace VersionAttributeTask
+{
+    using System;
+    using System.Reflection;
+
+    [Version("2.11")]
+    public class AttributesMain
+    {
+        public static void Main()
+        {
+            Type type = typeof(AttributesMain);
+            var attributes = type.GetCustomAttributes<VersionAttribute>();
+
+            foreach (var attribute in attributes)
+            {
+                Console.WriteLine(attribute.Version);
+            }
+        }
+    }
+}
---
+++ OOP/Homeworks/DefiningClassesPart2/VersionAttributeTask/VersionAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace VersionAttributeTask
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum |
+        AttributeTargets.Method | AttributeTargets.Struct)]
+
+    public class VersionAttribute : Attribute
+    {
+        private string version;
+
+        public VersionAttribute(string currentVersion)
+        {
+            this.Version = currentVersion;
+        }
+
+        public string Version
+        {
+            get
+            {
+                return this.version;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Version cannot be null or empty");
+                }
+
+                this.version = value;
+            }
+        }
+    }
+}
