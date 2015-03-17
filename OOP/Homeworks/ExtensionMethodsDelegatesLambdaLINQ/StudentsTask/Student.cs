8d9f1b9cf0a416c77a68695247963e1d7ff3adcf
zhenyaracheva <zhenia.racheva@gmail.com>
2015-03-17 21:25:36 +0200

    Update Problems: 3 - 5

--- OOP/Homeworks/ExtensionMethodsDelegatesLambdaLINQ/StudentsTask/Student.cs
+++ OOP/Homeworks/ExtensionMethodsDelegatesLambdaLINQ/StudentsTask/Student.cs
@@ -1,4 +1,15 @@
-﻿namespace StudentsTask
+﻿// Problem 3. First before last
+   
+// Write a method that from a given array of students finds all students whose first name is before its last name alphabetically.
+// Use LINQ query operators.
+// Problem 4. Age range
+   
+// Write a LINQ query that finds the first name and last name of all students with age between 18 and 24.
+// Problem 5. Order students
+   
+// Using the extension methods OrderBy() and ThenBy() with lambda expressions sort the students by first name and last name in descending order.
+// Rewrite the same with LINQ.
+namespace StudentsTask
 {
     using System;
     using System.Collections.Generic;
