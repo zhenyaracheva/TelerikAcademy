332981369511a023811542591b146e54d1a8e6a7
zhenyaracheva <zhenia.racheva@gmail.com>
2015-03-18 12:58:40 +0200

    Added Problem 7. Timer

---
+++ OOP/Homeworks/ExtensionMethodsDelegatesLambdaLINQ/Timer/Timer.cs
@@ -0,0 +1,34 @@
+﻿// Problem 7. Timer
+
+// Using delegates write a class Timer that can execute certain method at each t seconds.
+namespace Timer
+{
+    using System;
+    using System.Threading;
+
+    public delegate void TimerRepeat(DateTime time);
+
+    public class Timer
+    {
+        public static void Main()
+        {
+            Console.Write("Please, enter interval in seconds: ");
+            int seconds = int.Parse(Console.ReadLine());
+            Console.WriteLine(DateTime.Now);
+
+            TimerRepeat temp = new TimerRepeat(PrintDateTime);
+            {
+                while (true)
+                {
+                    Thread.Sleep(seconds * 1000);
+                    temp(DateTime.Now);
+                }
+            }
+        }
+
+        public static void PrintDateTime(DateTime date)
+        {
+            Console.WriteLine("{0}", DateTime.Now);
+        }
+    }
+}
