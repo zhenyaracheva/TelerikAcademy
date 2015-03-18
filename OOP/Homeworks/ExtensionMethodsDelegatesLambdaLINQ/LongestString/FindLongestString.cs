6ffea0d8ea12d20f108b258c9a9a7e3f112316cc
zhenyaracheva <zhenia.racheva@gmail.com>
2015-03-18 23:17:14 +0200

    Added Problem 17. Longest string

---
+++ OOP/Homeworks/ExtensionMethodsDelegatesLambdaLINQ/LongestString/FindLongestString.cs
@@ -0,0 +1,22 @@
+﻿// Problem 17. Longest string
+   
+// Write a program to return the string with maximum length from an array of strings.
+// Use LINQ.
+namespace LongestString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FindLongestString
+    {
+        public static void Main()
+        {
+            var listOfStrings = new List<string>() { "someString", "short", "min", "maxLenghtString", "anotherString", "justString" };
+            var maxLenghtString = listOfStrings.OrderByDescending(s => s.Length).ToList();
+            int maxLenght = maxLenghtString[0].Length;
+            var result = maxLenghtString.Where(s => s.Length == maxLenght);
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
