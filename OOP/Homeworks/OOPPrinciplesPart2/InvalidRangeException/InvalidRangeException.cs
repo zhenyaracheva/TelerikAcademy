a2ff0fe7bec8f998c3b9e2ef2f20bec96ce2286d
zhenyaracheva <zhenia.racheva@gmail.com>
2015-03-26 18:04:53 +0200

    Update

---
+++ OOP/Homeworks/OOPPrinciplesPart2/InvalidRangeException/InvalidRangeException.cs
@@ -0,0 +1,26 @@
+﻿namespace RangeException
+{
+    using System;
+
+    public class InvalidRangeException<T> : ApplicationException
+        where T : struct
+    {
+        public InvalidRangeException(string msg, T startRange, T endRange, Exception innerEx)
+            : base(msg, innerEx)
+        {
+            this.Start = startRange;
+            this.End = endRange;
+        }
+
+        public InvalidRangeException(string msg, T startRange, T endRange)
+            : this(msg, startRange, endRange, null)
+        {
+            this.Start = startRange;
+            this.End = endRange;
+        }
+
+        public T Start { get;  set; }
+
+        public T End { get;  set; }
+    }
+}
---
+++ OOP/Homeworks/OOPPrinciplesPart2/InvalidRangeException/InvalidRangeExceptionMain.cs
@@ -0,0 +1,39 @@
+﻿namespace RangeException
+{
+    using System;
+    using System.Globalization;
+
+    public class InvalidRangeExceptionMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please, enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 100)
+            {
+                throw new InvalidRangeException<int>("Number is out of range [1....100]", 1, 100);
+            }
+            else
+            {
+                Console.WriteLine("Number is not out of range [1...100]");
+            }
+
+            Console.WriteLine();
+            Console.Write("Please, enter a date in format dd/MM/yyyy: ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            DateTime start = new DateTime(1980, 1, 1);
+            DateTime end = new DateTime(2013, 12, 31);
+
+            if (date < start || date > end)
+            {
+                throw new InvalidRangeException<DateTime>("Date must be in range [1.1.1980] - [31.12.2013]", start, end);
+            }
+            else
+            {
+                Console.WriteLine("Date is not out of range[1.1.1980] - [31.12.2013]");
+            }
+        }
+    }
+}
