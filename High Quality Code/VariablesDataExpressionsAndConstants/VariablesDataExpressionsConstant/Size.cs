f20329e987748dfd9c47ea0d13ac98c395552e14
zhenyaracheva <zhenia.racheva@gmail.com>
2015-07-14 19:54:10 +0300

    [HW]

---
+++ High Quality Code/VariablesDataExpressionsAndConstants/VariablesDataExpressionsConstant/Size.cs
@@ -0,0 +1,70 @@
+﻿namespace VariablesDataExpressionsConstant
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double heigth;
+
+        public Size(double w, double h)
+        {
+            this.width = w;
+            this.heigth = h;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Size width cannot be negative number!");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Heigth
+        {
+            get
+            {
+                return this.heigth;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Size heigth cannot be negative number!");
+                }
+
+                this.heigth = value;
+            }
+        }
+
+        public static Size GetRotatedSize(Size size, double angle)
+        {
+            var width = RotateWidth(size, angle);
+            var heigth = RotateHeigth(size, angle);
+
+            return new Size(width, heigth);
+        }
+
+        private static double RotateWidth(Size size, double angle)
+        {
+            return (Math.Abs(Math.Sin(angle)) * size.heigth) + (Math.Abs(Math.Cos(angle)) * size.width);
+        }
+
+        private static double RotateHeigth(Size size, double angle)
+        {
+            return (Math.Abs(Math.Sin(angle)) * size.width) + (Math.Abs(Math.Cos(angle)) * size.heigth);
+        }
+    }
+}
---
+++ High Quality Code/VariablesDataExpressionsAndConstants/VariablesDataExpressionsConstant/VariablesDataExpressionsConstantsHW.cs
@@ -0,0 +1,39 @@
+﻿namespace VariablesDataExpressionsConstant
+{
+    using System;
+    using System.Linq;
+
+    public class VariablesDataExpressionsConstantsHW
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public void PrintMinMaxAverageElement(double[] numbers, int length)
+        {
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            double sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                var currentValue = numbers[i];
+                sum += currentValue;
+
+                if (currentValue > max)
+                {
+                    max = currentValue;
+                }
+
+                if (currentValue < min)
+                {
+                    min = currentValue;
+                }
+            }
+
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Average: " + (sum / length));
+        }
+    }
+}
